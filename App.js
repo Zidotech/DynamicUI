6e7cf6153212c7df5198271b4b8e37485132ca73
Zulu <zulu.devs@gmail.com>
2024-03-20 12:20:40 +0100

    learned about platform specific code

--- App.js
+++ App.js
@@ -1,11 +1,111 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Dimension Api
+
+// import { StyleSheet, View, Text, Dimensions } from "react-native";
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.box}>
+//         <Text style={styles.text}>Welcome!</Text>
+//       </View>
+//     </View>
+//   );
+// }
+
+// const windowWidth = Dimensions.get("window").width;
+// const windowHeight = Dimensions.get("window").height;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "plum",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   box: {
+//     width: windowWidth > 500 ? "70%" : "90%",
+//     height: windowHeight > 600 ? "60%" : "90%",
+//     backgroundColor: "lightblue",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   text: {
+//     fontSize: windowWidth > 500 ? 50 : 24,
+//   },
+// });
+
+// import { useEffect, useState } from "react";
+// import { StyleSheet, View, Text, Dimensions } from "react-native";
+
+// export default function App() {
+//   const [dimensions, setDimensions] = useState({
+//     window: Dimensions.get("window"),
+//   });
+
+//   useEffect(() => {
+//     const subscription = Dimensions.addEventListener("change", ({ window }) => {
+//       setDimensions({ window });
+//     });
+//     return () => subscription.remove();
+//   });
+
+//   const { window } = dimensions;
+//   const windowWidth = window.width;
+//   const windowHeight = window.height;
+//   return (
+//     <View style={styles.container}>
+//       <View
+//         style={[
+//           styles.box,
+//           {
+//             width: windowWidth > 500 ? "70%" : "90%",
+//             height: windowHeight > 600 ? "60%" : "90%",
+//           },
+//         ]}
+//       >
+//         <Text style={{ fontSize: windowWidth > 500 ? 50 : 24 }}>Welcome!</Text>
+//       </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "plum",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   box: {
+//     // width: windowWidth > 500 ? "70%" : "90%",
+//     // height: windowHeight > 600 ? "60%" : "90%",
+//     backgroundColor: "lightblue",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   // text: {
+//   //   fontSize: windowWidth > 500 ? 50 : 24,
+//   // },
+// });
+
+import { StyleSheet, View, Text, useWindowDimensions } from "react-native";
 
 export default function App() {
+  const windowWidth = useWindowDimensions().width;
+  const windowHeight = useWindowDimensions().height;
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View
+        style={[
+          styles.box,
+          {
+            width: windowWidth > 500 ? "70%" : "90%",
+            height: windowHeight > 600 ? "60%" : "90%",
+          },
+        ]}
+      >
+        <Text style={{ fontSize: windowWidth > 500 ? 50 : 24 }}>Welcome!</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +113,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "plum",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  box: {
+    // width: windowWidth > 500 ? "70%" : "90%",
+    // height: windowHeight > 600 ? "60%" : "90%",
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  // text: {
+  //   fontSize: windowWidth > 500 ? 50 : 24,
+  // },
 });
